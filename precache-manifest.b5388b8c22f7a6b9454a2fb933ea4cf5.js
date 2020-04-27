3202e0421bc5dc1e22ae47ea4c214181d3e83fb4
Mario Jimenez <mariojose.0732@gmail.com>
2020-04-27 17:30:50 -0400

    Updates

---
+++ precache-manifest.b5388b8c22f7a6b9454a2fb933ea4cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7d5980b6744556b5cd0dbec9baca77",
+    "url": "/quiz-react/index.html"
+  },
+  {
+    "revision": "d1ca97494d2a7d38b415",
+    "url": "/quiz-react/static/css/main.72fef309.chunk.css"
+  },
+  {
+    "revision": "7016cabf7852201725a4",
+    "url": "/quiz-react/static/js/2.36967a19.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/quiz-react/static/js/2.36967a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ca97494d2a7d38b415",
+    "url": "/quiz-react/static/js/main.ce46148f.chunk.js"
+  },
+  {
+    "revision": "caa51a4597b636f231f8",
+    "url": "/quiz-react/static/js/runtime-main.0979b2fa.js"
+  }
+]);
